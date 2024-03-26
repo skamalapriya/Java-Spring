0a9e602cd31faf2565c1f78d8bdd57a1915d60e3
skamalapriya <kamalapriyas1985@gmail.com>
2024-03-26 15:02:50 -0700

    Added SpringBootProject

---
+++ SpringBootAddToCartProject/src/main/java/com/code/springbootapi/AddToCartApiBootApplication.java
@@ -0,0 +1,13 @@
+package com.code.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddToCartApiBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddToCartApiBootApplication.class, args);
+	}
+
+}
---
+++ SpringBootAddToCartProject/src/test/java/com/code/springbootapi/AddToCartApiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddToCartApiBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
